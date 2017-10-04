9743a84467778525b2bf4924700fefe86c852c98
Deanna Lepke <deannalepke@gmail.com>
2017-10-04 21:43:08 +0000

    now sends number of logged in users (clients) to front end

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
-// server.js
-
 const express = require('express');
 const WebSocket = require('ws');
 const { Server, OPEN } = WebSocket;
 const uuid = require('uuid/v4');
  
-// Set the port to 3001
+
 const PORT = 3001;
  
-// Create a new express server
+
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', () => console.log(`Listening on ${ PORT }`));
  
-// Create the WebSockets server
-const wss = new Server({ server });
 
-function broadcast(payload) {
+const wss = new Server({ server }); //maybe change this to { server: app } ???
+
+function broadcast(payload) { //this is just the function declaration to be used later 
   const message = JSON.stringify(payload);
   wss.clients.forEach(client => {
     if(client.readyState !== OPEN) { return; }
@@ -26,20 +23,29 @@ function broadcast(payload) {
   })
 }
  
-// Set up a callback that will run when a client connects to the server
-// When a client connects they are assigned a socket, represented by
-// the ws parameter in the callback.
+
 wss.on('connection', (ws) => {
   console.log('Client connected');
+  const clientInfoOnConnection = {
+    numClients: wss.clients.size,
+    type: 'clientCount'
+  };
+  broadcast(clientInfoOnConnection);
 
   ws.on('message', (messageJSON) => {
-    const message = JSON.parse(messageJSON);
+    const message = JSON.parse(messageJSON); //this parses the JSON string that contains the new message info
     const id = uuid();
     message.id = id;
 
-    broadcast({ message });
+    broadcast({ message }); //this is where the new message gets sent to everyone
   });
  
-  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-  ws.on('close', () => console.log('Client disconnected'));
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    const clientInfoOnClose = {
+      numClients: wss.clients.size,
+      type: 'clientCount'
+    };
+    broadcast(clientInfoOnClose);
+  })
 });
