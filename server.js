0c99c3e5e1f798ab7b8ecd2fd66a8a4548e25fbc
Deanna Lepke <deannalepke@gmail.com>
2017-10-05 01:37:51 +0000

    added unique font colors for each username

--- server.js
+++ server.js
@@ -23,19 +23,31 @@ function broadcast(payload) { //this is just the function declaration to be used
   })
 }
  
+let number = 0;
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
+  const colorArray = ["#24bd14", "#d31010", "#204cea", "#db10f2"];
+  function pickColor() {
+    if (number > 2) {
+      number = -1;
+    }
+    number += 1;
+    return colorArray[number];
+  }
   const clientInfoOnConnection = {
     numClients: wss.clients.size,
-    type: 'clientCount'
+    type: 'clientCount',
+    color: pickColor()
   };
+  
   broadcast(clientInfoOnConnection);
 
   ws.on('message', (messageJSON) => {
     const message = JSON.parse(messageJSON); //this parses the JSON string that contains the new message info
     const id = uuid();
     message.id = id;
+    message.color = clientInfoOnConnection.color;
 
     broadcast({ message }); //this is where the new message gets sent to everyone
   });
