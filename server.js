2446b3e9db81042d3c82b0f2e74b89c48eda72e9
Deanna Lepke <deannalepke@Deannas-MacBook-Pro.local>
2017-10-05 12:51:00 -0700

    fixed formatting (spaces n such)

--- server.js
+++ server.js
@@ -2,27 +2,27 @@ const express = require('express');
 const WebSocket = require('ws');
 const { Server, OPEN } = WebSocket;
 const uuid = require('uuid/v4');
- 
+
 
 const PORT = 3001;
- 
+
 
 const server = express()
   .use(express.static('public'))
-  .listen(PORT, '0.0.0.0', () => console.log(`Listening on ${ PORT }`));
- 
+  .listen(PORT, '0.0.0.0', () => console.log(`Listening on ${PORT}`));
+
 
 const wss = new Server({ server }); //maybe change this to { server: app } ???
 
 function broadcast(payload) { //this is just the function declaration to be used later 
   const message = JSON.stringify(payload);
   wss.clients.forEach(client => {
-    if(client.readyState !== OPEN) { return; }
+    if (client.readyState !== OPEN) { return; }
 
     client.send(message);
   })
 }
- 
+
 let number = 0;
 
 wss.on('connection', (ws) => {
@@ -40,7 +40,7 @@ wss.on('connection', (ws) => {
     type: 'clientCount',
     color: pickColor()
   };
-  
+
   broadcast(clientInfoOnConnection);
 
   ws.on('message', (messageJSON) => {
@@ -51,7 +51,7 @@ wss.on('connection', (ws) => {
 
     broadcast({ message }); //this is where the new message gets sent to everyone
   });
- 
+
   ws.on('close', () => {
     console.log('Client disconnected');
     const clientInfoOnClose = {
